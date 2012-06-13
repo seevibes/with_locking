65fcdf3a6cd6e5c05daa59daae67c8aa0a832e1e
Marty Kraft <martinkraft@gmail.com>
2012-06-12 20:58:59 -0400

    Added methods and specs. Moved license because it's already in its own file.

--- lib/with_locking.rb
+++ lib/with_locking.rb
@@ -1,5 +1,38 @@
 require "with_locking/version"
 
-module WithLocking
-  # Your code goes here...
+module WithLocking 
+
+  def self.do(options = {}, &block)
+    raise "No block given" unless block_given?
+
+    name = options[:name] || "locking_service_task"
+    pid_file = "tmp/pids/#{name}.pid"
+      
+    return false if File.exists? pid_file
+
+    begin 
+      Dir.mkdir("tmp")
+      Dir.mkdir("tmp/pids")
+    rescue Errno::EEXIST => e
+      # ignore this error
+    end
+      
+    File.open(pid_file, 'w') { |f| f.puts Process.pid }
+      
+    begin
+      block.call
+    ensure
+      File.delete pid_file
+    end
+    return true
+  end
+
+  def self.do!(options = {}, &block)
+    raise "locked process still running" unless self.do(options, &block)
+  end
+
+  def self.locked?(name)
+    File.exists?("tmp/pids/#{name}.pid")
+  end
+
 end
--- spec/with_locking_spec.rb
+++ spec/with_locking_spec.rb
@@ -1,7 +1,54 @@
 require_relative '../lib/with_locking.rb'
 
 describe WithLocking do
-  it "should do blah" do
-    true.should == true
+  let(:path) { 'tmp/pids/rspec_test.pid' }
+  let(:file_name) { 'rspec_test' }
+
+  before :each do
+    File.stub(:open)
+    File.stub(:delete)
+  end
+
+  describe "#do" do
+
+    it "writes a pid file" do
+      File.should_receive(:open).with(path, "w")
+      WithLocking.do(:name => file_name) { }
+    end
+
+    it "executes the given block" do
+      my_test_array = []
+      WithLocking.do { my_test_array << 1 }
+      my_test_array.count.should == 1
+    end
+
+    it "deletes the pid file" do
+      File.should_receive(:delete).with(path)
+      WithLocking.do(:name => file_name) {}
+    end
+
+    it "should not execute the block if the pid file already exists" do
+      File.stub(:exists?).and_return(true)
+      WithLocking.do {}.should == false
+    end
+
+  end
+
+  describe "#do!" do
+
+    it "raises an exception if the pid file already exists" do
+      File.stub(:exists?).and_return(true)
+      lambda { WithLocking.do! {} }.should raise_error(RuntimeError, 'locked process still running')
+    end
+
+  end
+
+  describe "#locked?" do
+
+    it "checks if the file exists" do
+      File.should_receive(:exists?).with(path)
+      WithLocking.locked?(file_name)
+    end
+
   end
 end
